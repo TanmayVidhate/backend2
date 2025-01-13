9dd99aafc3a8e9251300aa62275ac47abc0870fd
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-13 10:42:06 +0530

    Add dotenv file

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { getHealth } from './controllers/Health.js';
 import { getStudent, getStudentById, postStudent, deleteStudentById, patchStudentById } from "./controllers/Student.js"
 const app = express();
 
+
+
 app.use(cors());
 app.use(express.json());
 
-
 app.get("/health", getHealth);
-
 
 app.get("/students", getStudent)
 
@@ -17,14 +20,12 @@ app.get("/students/:id", getStudentById)
 
 app.post("/students", postStudent)
 
-
 app.delete("/students/:id", deleteStudentById)
-
 
 app.patch("/students/name/:id", patchStudentById);
 
 
-const PORT = 5003;
+const PORT = process.env.PORT||5003;
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
